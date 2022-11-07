1b89d53012619008d996f7cecb0a20f285dfa607
Kirti Poddar <kirtipoddar151@gmail.comm>
2022-11-07 22:21:14 +0530

    add: main.js

---
+++ main.js
@@ -0,0 +1,139 @@
+//create object
+const user={
+    name: 'John',
+    age: 30
+};
+
+console.log(user.name);
+
+//assign new property
+user.country='USA';
+
+console.log(user);
+
+//changing a property
+user.age=45;
+console.log(user);
+
+//destructuring
+const {age,name}=user;
+
+console.log(age);
+
+//references (bad copy)
+const user2=user;
+console.log(user2);
+
+user2.name='Peter';
+
+console.log(user2);
+console.log(user);
+
+//spread operator (good copy)
+const user3={...user};
+user3.name='Mary';
+
+console.log(user3);
+console.log(user);
+
+
+
+
+
+
+
+
+
+
+
+
+// //if statement
+// if(0.5>1) return console.log(true);
+
+// //ternary statement
+// 0.5>1 ? console.log(true) : console.log(false);
+
+// //logical and operator
+// (2.5>1) && console.log(true);
+
+// (1==='1')?console.log(true):console.log(false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //function declaration
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(add(1,2));
+// //arrow functions
+// const sum=(a,b)=>a+b;
+// console.log(sum(1,2));
+
+
+
+
+
+
+// //string
+// const typeString='Kirti';
+
+// //numbers
+// const typeNumber=13233;
+
+// //boolean
+// const typeboolean=true;
+
+// //undefined
+// let myName;
+
+// //null 
+// const typeNull=null;
+
+// console.log(
+//     typeof typeString,
+//     typeof typeNumber,
+//     typeof typeboolean,
+//     typeof myName,
+//     typeof typeNull
+// );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let myName='Kirti';
+
+// myName='John';
+// // console.log('Hi, my  name is '+myName);
+// console.log(`Hi, my name is ${myName}, I'm 20 years old.`);
